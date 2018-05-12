024f307c91d966ba4bd085748a8f58b6d833dbfe
Lyndon Mackay <lyndondmackay@gmail.com>
2018-05-12 14:52:42 +1000

    Added ability to Listen and not listen, also sending towards event

--- Calculator/VoiceFiles/Voice.cs
+++ Calculator/VoiceFiles/Voice.cs
@@ -10,25 +10,25 @@ namespace Calculator.VoiceFiles
     class Voice
     {
 
-        SpeechRecognitionEngine recognizer =
+        SpeechRecognitionEngine _recognizer =
       new SpeechRecognitionEngine(
         new System.Globalization.CultureInfo("en-NZ"));
-
+        bool _listening = false;
 
         public Voice(EventHandler<SpeechRecognizedEventArgs> result)
         {
             Choices numbers = new Choices(new string[] { "1", "2", "4", "5", "8", "9" });
             //The numbers that on testing where most frequently misheard
             Choices harderNumbers = new Choices(new string[] { "2", "3", "6", "7" });
-            recognizer.LoadGrammar(CreateGrammer(numbers));
+            _recognizer.LoadGrammar(CreateGrammer(numbers));
 
             Grammar hardGrammer = CreateGrammer(harderNumbers);
             hardGrammer.Priority = 127;//Max
-            recognizer.LoadGrammar(hardGrammer);
+            _recognizer.LoadGrammar(hardGrammer);
             // Configure input to the speech recognizer.
-            recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.SetInputToDefaultAudioDevice();
 
-            recognizer.SpeechRecognized +=result ;
+            _recognizer.SpeechRecognized +=result ;
         }
         private Grammar CreateGrammer(Choices numbers)
         {
@@ -38,9 +38,18 @@ namespace Calculator.VoiceFiles
         }
         public void Listen()
         {
-
+            if(_listening)
+            {
+                return;
+            }
             // Start asynchronous, continuous speech recognition.
-            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            _listening = true;
+        }
+        public void StopListening()
+        {
+            _listening = false;
+            _recognizer.RecognizeAsyncStop();
         }
        
     }
