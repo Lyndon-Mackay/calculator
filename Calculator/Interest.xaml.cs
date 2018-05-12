61b7329b2681ab45c959297bd3f8a291e015cac1
Lyndon Mackay <lyndondmackay@gmail.com>
2018-05-12 23:43:58 +1000

    Added labels, label overwriting and changed doubles to decimals since this is dealing in money

--- Calculator/Interest.xaml.cs
+++ Calculator/Interest.xaml.cs
@@ -21,11 +21,16 @@ namespace Calculator
     /// </summary>
     public partial class Interest : Page
     {
-        const double SMALL_LOAN_ESTABLIHSMENT_PERCENT = 0.2;
-        const double MEDIUM_LOAN_ESTABLISHMENT_FEE = 400;
-        const double MINIMUM_LOAN = 300.0;
-        const double MEDIUM_LOAN = 2001.0;
-        const double MAXIMUM_LOAN = 5000.0;
+        /***
+         * Constanst from nimble website
+         */ 
+        const decimal SMALL_LOAN_ESTABLIHSMENT_PERCENT = 0.2m;
+        const decimal MEDIUM_LOAN_ESTABLISHMENT_FEE = 400m;
+        const decimal SMALL_LOAN_MONTHLY_FEE = .04m;
+        const decimal MINIMUM_LOAN = 300.0m;
+        const decimal MEDIUM_LOAN = 2001.0m;
+        const decimal MEDIUM_ANNUAL_INTEREST_RATE = .476158m;
+        const decimal MAXIMUM_LOAN = 5000.0m;
         const int INSERT_ERROR_INDEX = 2;
         public Interest()
         {
@@ -34,65 +39,78 @@ namespace Calculator
         private void BtnSubmit_Click(object sender, RoutedEventArgs e)
         {
             string numberString = InputTextBox.Text;
-            double loanAmount = double.Parse(numberString);
+            decimal loanAmount = decimal.Parse(numberString);
 
             if (loanAmount < MINIMUM_LOAN)
             {
+                string errorString = String.Format("The minimum loan amount allowed is ${0}", MINIMUM_LOAN);
 
-                SetErrorLabel(String.Format("The minimum loan amount allowed is ${0}", MINIMUM_LOAN));
+                SetDynamicLabel("ErrorLabel",errorString);
 
             }
             else if (loanAmount < MEDIUM_LOAN)
             {
                 EstablishmentTextBox.Text = (SMALL_LOAN_ESTABLIHSMENT_PERCENT * loanAmount).ToString();
+
+                decimal monthlyLoanAmount = loanAmount * SMALL_LOAN_MONTHLY_FEE;
+
+                string repaymentString = String.Format("Your monthly loan fee is ${0}", monthlyLoanAmount);
+                SetDynamicLabel("RepaymentLabel", repaymentString);
             }
             else if (loanAmount <= MAXIMUM_LOAN)
             {
                 EstablishmentTextBox.Text = MEDIUM_LOAN_ESTABLISHMENT_FEE.ToString();
+
+                decimal yearlyLoanAmount = loanAmount * MEDIUM_ANNUAL_INTEREST_RATE;
+
+                string repaymentString = String.Format("Your yearly Interest charge is ${0}", yearlyLoanAmount);
+                SetDynamicLabel("RepaymentLabel", repaymentString);
             }
             else
             {
-                SetErrorLabel(String.Format("The maximum loan amount allowed is ${0}", MAXIMUM_LOAN));
+                string errorString = String.Format("The maximum loan amount allowed is ${0}", MAXIMUM_LOAN);
+                SetDynamicLabel("ErrorLabel",errorString);
             }
         }
         /// <summary>
-        /// Puts an error label into the form
+        /// Overwrites a previous label or creates a new one
+        /// if it does not exists
         /// </summary>
-        /// <param name="error">The erros message to put</param>
-        private void SetErrorLabel(string error)
+        /// <param name="labelName">The name of the label to overwrite or create</param>
+        /// <param name="TextContent">The text the new label should display</param>
+        private void SetDynamicLabel(string labelName,string TextContent)
+        {
+            Label repaymentRate = null;
+            repaymentRate = TryFindPreviousLabel(labelName);
+            if(repaymentRate == null)
+            {
+                repaymentRate = new Label
+                {
+                    Name = labelName,
+                    Content = TextContent
+                };
+                LoanInput.Children.Add(repaymentRate);
+            }
+            else
+            {
+                repaymentRate.Content = TextContent;
+            }
+
+        }
+        /// <summary>
+        /// trys to find a previous label if it exists
+        /// </summary>
+        /// <param name="name">the name of the label to find</param>
+        /// <returns>The label if found otherwise null</returns>
+        private Label TryFindPreviousLabel(string name)
         {
             Label label = null;
-            //NO LINQ  :( !!
-            label = TryFindPreviousErrorLabel(label);
-            if (label == null)
-            {
-                label = new Label
-                {
-                    Name = "ErrorLabel",
-                    Content = error
-                };
-                LoanInput.Children.Insert(INSERT_ERROR_INDEX, label);
-            }
-            else
-            {
-                label.Content = error;
-            }
-
-        }
-
-        /// <summary>
-        /// trys to find a previous error label if it exists
-        /// </summary>
-        /// <param name="label">a label alue will be overwritten by the new error label if found</param>
-        /// <returns>The label if found otherwise null</returns>
-        private Label TryFindPreviousErrorLabel(Label label)
-        {
             foreach (UIElement child in LoanInput.Children)
             {
                 if (child is Label)
                 {
                     Label labelChild = child as Label;
-                    if (labelChild.Name.Equals("ErrorLabel"))
+                    if (labelChild.Name.Equals(name))
                     {
                         label = child as Label;
                         break;
