041074b0a4a6fc7ec1bf215dc8e68997799671af
Lyndon Mackay <lyndondmackay@gmail.com>
2018-05-12 13:33:19 +1000

    Initital Calculator

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using org.mariuszgromada.math.mxparser;
 namespace Calculator
 {
     /// <summary>
@@ -20,9 +20,81 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool OverWriteDisplay = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// Handles the simple case where the buttons are
+        /// added to the text dislay for numbers and the basic operations
+        /// </summary>
+        /// <param name="sender">The button</param>
+        /// <param name="e">Boilerplate state information crrently unused</param>
+        private void BtnNumberAndOperationClick(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            if (OverWriteDisplay)
+            {
+                txtDisplay.Text = b.Content.ToString();
+                OverWriteDisplay = false;
+            }
+            else
+            {
+                txtDisplay.Text += b.Content.ToString();
+                
+            }
+        }
+        /// <summary>
+        /// Handles the event when the equals buton is pressed
+        /// </summary>
+        /// <param name="sender">the equals button</param>
+        /// <param name="e">Boilerplate state information crrently unused</param>
+        private void BtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            string evaluatedText = txtDisplay.Text;
+            //have to use fullnamespace as it conflicts with windows :(
+            org.mariuszgromada.math.mxparser.Expression expression = new org.mariuszgromada.math.mxparser.Expression(evaluatedText);
+            double result = expression.calculate();
+            if(Double.IsNaN(result))
+            {
+                txtDisplay.Text = "Error";
+            }
+            else
+            {
+                txtDisplay.Text = result.ToString();
+                HistoryText.Items.Add(evaluatedText);
+                OverWriteDisplay = true;
+            }
+        }
+
+        private void HistoryText_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListBox historyBox = sender as ListBox;
+            if(historyBox.SelectedItem != null)
+            {
+                txtDisplay.Text = historyBox.SelectedItem.ToString();
+                OverWriteDisplay = false;
+            }
+        }
+
+        private void BtnBackSpace_Click(object sender, RoutedEventArgs e)
+        {
+            //subtract the last character
+            txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
+        }
+
+        private void BtnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            HistoryText.Items.Clear();
+        }
+
+        private void BtnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            OverWriteDisplay = false;
+            txtDisplay.Clear();
         }
     }
 }
